6b1e89f646cbb85fa268062290c1dab103ee735b
Rogerio Vieira <rogeriovbh@gmail.com>
2022-11-03 22:26:15 -0300

    Altera campo de purchase date

--- baseproject/comics/models.py
+++ baseproject/comics/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from uuid import uuid4
 
-# Create your models here.
 
 class Comics(models.Model):
   id_comic = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   title = models.CharField(max_length=255)
   author = models.CharField(max_length=255)
-  purchase_date = models.IntegerField()
+  purchase_date = models.DateField()
   read = models.BooleanField()
   is_reading = models.BooleanField()
   created_at = models.DateTimeField(auto_now_add=True)
