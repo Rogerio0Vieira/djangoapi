a13b4f7edbe71fc2b08fc0518382163de74812a2
Rogerio Vieira <rogeriovbh@gmail.com>
2023-02-25 21:45:04 -0300

    Inicia componente de botao generico

---
+++ lombadeiro/src/components/GenericButton/GenericButton.tsx
@@ -0,0 +1,9 @@
+import GenericButtonStyled from './styles';
+
+interface GenericButtonProps {
+  name: string;
+}
+
+export const GenericButton () =>{
+
+}
---
+++ lombadeiro/src/components/GenericButton/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const GenericButtonStyled = styled.button`
+  width: 50px;
+  background-color: ${props => props.color};
+`;
+
+export default GenericButtonStyled;
