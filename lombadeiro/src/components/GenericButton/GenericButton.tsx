4a543f850bda1478d03847a12fe84f6939251ea5
Rogerio Vieira <rogeriovbh@gmail.com>
2023-02-25 21:47:48 -0300

    Implementa botao generico

--- lombadeiro/src/components/GenericButton/GenericButton.tsx
+++ lombadeiro/src/components/GenericButton/GenericButton.tsx
@@ -4,6 +4,11 @@ interface GenericButtonProps {
   name: string;
 }
 
-export const GenericButton () =>{
+export const GenericButton: React.FC<GenericButtonProps> = ({name}) =>{
 
-}
+  return(
+    <GenericButtonStyled color='black'>
+      Teste
+    </GenericButtonStyled>
+  )
+};
