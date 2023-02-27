22eec327c1a3169b86ac8e4562b0a963b21dcacd
Rogerio Vieira <rogeriovbh@gmail.com>
2023-02-27 01:10:10 -0300

    Altera estilo do botao para padrao do projeto

--- lombadeiro/src/components/GenericButton/styles.ts
+++ lombadeiro/src/components/GenericButton/styles.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
 const GenericButtonStyled = styled.button`
-  width: 50px;
+  padding: 10px;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
   background-color: ${props => props.color};
+  color: #999;
 `;
 
 export default GenericButtonStyled;
