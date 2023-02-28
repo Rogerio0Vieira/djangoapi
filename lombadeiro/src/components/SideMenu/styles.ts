0593b7871041cf39555a9722c641aafdcd675401
Rogerio Vieira <rogeriovbh@gmail.com>
2023-02-27 22:50:38 -0300

    Inicia arquivo de estilizacao

---
+++ lombadeiro/src/components/SideMenu/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const SideMenuStyled = styled.div`
+  padding: 10px;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
+  width: 150px;
+
+  ::placeholder {
+    color: #999;
+  }
+
+ 
+
+`;
