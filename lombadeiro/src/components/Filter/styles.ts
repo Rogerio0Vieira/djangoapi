d1bbbeb8062ddf6a95d69f80cd6c1cd3e480a779
Rogerio Vieira <rogeriovbh@gmail.com>
2023-03-04 21:08:27 -0300

    implementa estilizacao

---
+++ lombadeiro/src/components/Filter/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
+
+const Label = styled.label`
+  margin-bottom: 8px;
+  font-weight: bold;
+`;
+
+const Select = styled.select`
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  color: #333;
+`;
