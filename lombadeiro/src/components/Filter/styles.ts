0740d756855836e45d10955a3440e11894f7833b
Rogerio Vieira <rogeriovbh@gmail.com>
2023-03-05 20:15:23 -0300

    adiciona export aos componentes estilizados

--- lombadeiro/src/components/Filter/styles.ts
+++ lombadeiro/src/components/Filter/styles.ts
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
-const FilterContainer = styled.div`
+export const FilterContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 16px;
 `;
 
-const Label = styled.label`
+export const Label = styled.label`
   margin-bottom: 8px;
   font-weight: bold;
 `;
 
-const Select = styled.select`
+export const Select = styled.select`
   padding: 8px;
   border-radius: 4px;
   border: 1px solid #ccc;
