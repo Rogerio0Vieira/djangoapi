c7d7d1ad21120970350d1c112dd0d04e13f34340
Rogerio Vieira <rogeriovbh@gmail.com>
2023-03-11 22:51:41 -0300

    adiciona componentes para teste

--- lombadeiro/src/App.tsx
+++ lombadeiro/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import { BookTumblr } from './components/BookTumblr/BookTumblr'
 import { SearchField } from './components/SearchField/SearchField'
 import { GenericButton } from './components/GenericButton/GenericButton'
+import { SideMenu } from './components/SideMenu/SideMenu'
+import { Filter } from './components/Filter/Filter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,8 @@ function App() {
   return (
     <div className="App">
       {/* <BookTumblr favorite={true} imageName='teste' imagePath='teste' title='teste'/> */}
+      <SideMenu/>
+      <Filter></Filter>
       <SearchField/>
       <GenericButton name="teste" color="purple"/>
     </div>
