72fa135d9a773d8a9d05d5ac0ed870f34a6b26d4
Rogerio Vieira <rogeriovbh@gmail.com>
2023-03-17 19:42:11 -0300

    sem vontade de fazer nada hoje

--- lombadeiro/src/App.tsx
+++ lombadeiro/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       {/* <BookTumblr favorite={true} imageName='teste' imagePath='teste' title='teste'/> */}
+      
       <SideMenu/>
         <Filter/>
       <SearchField/>
